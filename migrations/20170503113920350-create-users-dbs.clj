0be045ec840996aed65efc585d79a8a9139949f2
Will Curry <watercrafthd@gmail.com>
2017-05-03 14:52:31 +0100

    Add migration script for new RBAC data model

---
+++ migrations/20170503113920350-create-users-dbs.clj
@@ -0,0 +1,16 @@
+;; migrations/20170503113920350-create-users-dbs.clj
+
+(defn up []
+  ["DROP TABLE IF EXISTS users"
+   "CREATE TABLE roles(role_id serial PRIMARY KEY, name text)"
+   "CREATE TABLE users(user_id serial PRIMARY KEY, email text)"
+   "CREATE TABLE permissions(permission_id serial PRIMARY KEY, name text)"
+   "CREATE TABLE user_roles(user_id int references users(user_id), role_id int references roles(role_id))"
+   "CREATE TABLE role_permissions(role_id int references roles(role_id), permission_id int references permissions(permission_id))"])
+
+(defn down []
+  ["DROP TABLE roles"
+  "DROP TABLE users"
+  "DROP TABLE permissions"
+  "DROP TABLE user_roles"
+  "DROP TABLE role_permissions"])
