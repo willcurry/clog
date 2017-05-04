4ad2c7857ff5a3cd2db5904239bdd083be30d33d
Will Curry <watercrafthd@gmail.com>
2017-05-04 11:20:33 +0100

    Change tests to work with new data types

--- spec/clog/users_spec.clj
+++ spec/clog/users_spec.clj
@@ -6,29 +6,30 @@
 (describe "users"
   (around [it] (with-redefs [pg-db "postgresql://localhost:5432/clog_test"] (it))) 
 
+  (before
+    (sql/insert! pg-db :users
+      {:email "wcurry@8thlight.com"})
+    (sql/insert! pg-db :roles
+      {:name "admin"})
+    (sql/insert! pg-db :permissions
+      {:permission_id "a" :name "all"})
+    (sql/insert! pg-db :user_roles
+      {:user_id (users/user-id "wcurry@8thlight.com") :role_id (users/role-id-from "admin")})
+    (sql/insert! pg-db :role_permissions
+      {:permission_id "a" :role_id (users/role-id (users/user-id "wcurry@8thlight.com"))}))
+
   (after
     (sql/db-do-commands pg-db "truncate table user_roles")
     (sql/db-do-commands pg-db "truncate table role_permissions")
     (sql/db-do-commands pg-db "truncate table users cascade")
     (sql/db-do-commands pg-db "truncate table roles cascade")
     (sql/db-do-commands pg-db "truncate table permissions cascade"))
-
-  (before
-    (sql/insert! pg-db :users
-      {:user_id 1 :email "wcurry@8thlight.com"})
-    (sql/insert! pg-db :roles
-      {:role_id 1 :name "admin"})
-    (sql/insert! pg-db :permissions
-      {:permission_id 1 :name "all"})
-    (sql/insert! pg-db :user_roles
-      {:user_id 1 :role_id 1})
-    (sql/insert! pg-db :role_permissions
-      {:permission_id 1 :role_id 1}))
   
   (it "saves users"
+    (users/save "test@test.com")
     (should= 1
       (count (sql/query pg-db
-        ["select * from users where email='wcurry@8thlight.com'"]))))
+        ["select * from users where email='test@test.com'"]))))
   
   (it "gets a users role from their email"
     (should= "admin"
--- src/clog/users.clj
+++ src/clog/users.clj
@@ -4,17 +4,21 @@
 (def pg-db 
   "postgresql://localhost:5432/clog")
 
-(defn- save [email role]
+(defn save [email]
   (sql/insert! pg-db :users
     {:email email}))
 
-(defn- user-id [email]
+(defn user-id [email]
   (:user_id (first (sql/query pg-db
-    ["select * from users where email=?" email]))))
+    ["select user_id from users where email=?" email]))))
 
-(defn- role-id [user-id]
+(defn role-id [user-id]
   (:role_id (first (sql/query pg-db
     ["select role_id from user_roles where user_id=?" user-id]))))
+
+(defn role-id-from [name]
+  (:role_id (first (sql/query pg-db
+    ["select role_id from roles where name=?" name]))))
 
 (defn role-for [email]
   (let [role (:name (first (sql/query pg-db
